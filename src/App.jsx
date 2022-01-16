8c4ead174b39c1d145837efd754d945b25f5e113
yl <yl.code@qq.com>
2022-01-16 15:06:06 +0800

    some demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { JsxDemo } from "./base-demo/JsxDemo";
+// import { JsxDemo } from "./base-demo/JsxDemo";
+// import { ClassComDemo } from "./base-demo/ClassComDemo";
+// import { FuncComDemo } from "./base-demo/FuncComDemo";
+import { SetStateDemo } from "./base-demo/SetStateDemo";
 
 export class App extends Component {
   render() {
@@ -7,7 +10,13 @@ export class App extends Component {
       <div style={{ textAlign: "center" }}>
         <h1>App</h1>
 
-        <JsxDemo></JsxDemo>
+        {/* <JsxDemo></JsxDemo> */}
+
+        {/* <ClassComDemo></ClassComDemo> */}
+
+        {/* <FuncComDemo></FuncComDemo> */}
+
+        <SetStateDemo></SetStateDemo>
       </div>
     );
   }
