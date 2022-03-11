2862c8049f7e0a9104875c8ddd565ec41a58f1eb
yl <yl.code@qq.com>
2022-03-11 23:27:54 +0800

    [my-react-redux]: 文件结构变更

--- src/super-demo/react-redux-demo/index.js
+++ src/super-demo/react-redux-demo/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { ReactReduxHookPage } from "./react-redux-page/ReactReduxHookPage";
-import { ReactReduxPage } from "./react-redux-page/ReactReduxPage";
-import { store } from "./react-redux-page/store";
+import React from 'react';
+import { ReactReduxHookPage } from './ReactReduxHookPage';
+import { ReactReduxPage } from './ReactReduxPage';
+import { store } from './store';
 
 // import { Provider } from "react-redux";
-import { Provider } from "./react-redux-page/my-react-redux";
+import { Provider } from './react-redux-page/my-react-redux';
 
 export function ReactReduxDemo() {
   return (
     <Provider store={store}>
-      <ReactReduxPage ownProps={"ownProps"} />
+      <ReactReduxPage ownProps={'ownProps'} />
       <hr />
-      <ReactReduxHookPage ownProps={"ownProps"} />
+      <ReactReduxHookPage ownProps={'ownProps'} />
     </Provider>
   );
 }
